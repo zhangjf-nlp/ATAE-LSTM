20c5355bdb8ad828945433d92395ea7d48e8013e
zhangjf-nlp <1061683512@qq.com>
2020-11-17 17:28:01 +0800

    update many files and add real support for mini-batch training

---
+++ config.py
@@ -0,0 +1,58 @@
+#coding:utf-8
+import warnings
+class DefaultConfig(object):
+    env = 'ATAE-LSTM' # visdom环境
+    model = 'ATAE-LSTM' # 使用的模型，名字与 models/__init__.py 中一致
+    
+    base_root = 'D:/Jupyter/ATAE-LSTM/'
+    embedding_root = base_root + 'data//glove/glove.840B.300d.txt' # 预训练词向量路径
+    train_data_root = base_root + 'data/restaurants-train.xml' # 训练集路径
+    test_data_root = base_root + 'data/restaurants-trial.xml' # 测试集路径
+    load_model_path = None # 加载预训练模型路径 None表示不加载
+    
+    use_cuda = False
+    
+    word_independence = 5
+    # how many times an un-pretrained word have to appear for to be independent
+    # this allow out-of-vocabulary words to have an independent embedding instead of <UNKNOWN>
+    word_max_input = 100000
+    # the Stanford-GloVe file may contains too many pretrained word embeddings and most of them are not needed in this task
+    # so I can limit the number of words loaded to save memory
+    
+    max_seq_len = 128
+    # the sequence of tokens should not be more than max_seq_len in length
+    # the empty positions will be filled by token <PADDING>
+    max_terms_len = 32
+    
+    classes = 3 # how many classes
+    
+    batch_size = 4
+    gpu_list = None
+    
+    hidden_size = 300 # the dimension of word vectors, aspect embeddings and the size of hidden layers
+    
+    print_freq = 100 # how many batch as an interval between two prints
+    
+    max_epoch = 50
+    
+    lr = 0.01
+    lr_decay = 0.9 # momentum
+    weight_decay = 0.001 # L2-regularization
+    epsilon = 0.01 # unknown parameters are randomly initialized from U(−ϵ,ϵ)
+    
+    def parse(self, kwargs):
+        '''
+        根据字典 kwargs 更新 config 参数
+        '''
+        for k, v in kwargs.items():
+            if not hasattr(self, k):
+                warnings.warn("Warning : opt has not attribute named %s" %k)
+            setattr(self, k, v)
+        
+        print('user config:')
+        for k, v in self.__class__.__dict__.items():
+            if not k.startswith('__'):
+                print(k, getattr(self, k))
+        return
+
+opt = DefaultConfig()
